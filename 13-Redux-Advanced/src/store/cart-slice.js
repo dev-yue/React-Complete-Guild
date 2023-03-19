222c4d81e45ed52388bc0abf2d104bf73f9f0cdd
Adamyuyue <67887367+Adamyuyue@users.noreply.github.com>
2023-03-18 23:05:51 -0500

    added redux advanced

---
+++ 13-Redux-Advanced/src/store/cart-slice.js
@@ -0,0 +1,54 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    changed: false,
+    // totalAmount: 0,
+  },
+  reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items
+    },
+    addItemToCart(state, action) {
+      const newitem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newitem.id);
+      state.totalQuantity++;
+      state.changed = true;
+      if (!existingItem) {
+        state.items.push({
+          id: newitem.id,
+          price: newitem.price,
+          quantity: 1,
+          totalPrice: newitem.price,
+          name: newitem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newitem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      state.changed = true;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+
+
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
