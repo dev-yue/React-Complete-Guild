96a3e13a7a35884259e022805f44a0cffc029f6c
Adamyuyue <67887367+Adamyuyue@users.noreply.github.com>
2023-03-24 02:37:57 -0500

    added authentication app

---
+++ 16-authentication/frontend/src/pages/Root.js
@@ -0,0 +1,41 @@
+import { Outlet, useSubmit } from "react-router-dom";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
+import { useLoaderData } from "react-router";
+
+import MainNavigation from "../components/MainNavigation";
+
+function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
+  // const navigation = useNavigation();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration)
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
+
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
